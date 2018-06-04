134e68dbe6c2b52165ec1ace29bd439ff065fa30
Hisamitsu Okabe <okabe@worksap.co.jp>
2018-06-04 18:17:52 +0900

    feat: Finish loading page

--- __tests__/spa.js
+++ __tests__/spa.js
@@ -1,12 +1,20 @@
 const timeout = 5000;
 
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
 describe(
     '/spa-manager/index.html',
     () => {
         let page;
         beforeAll(async () => {
-            page = await global.__BROWSER__.newPage()
-            await page.goto('https://syusyu.github.io/spa-manager/src/index.html')
+            page = await global.__BROWSER__.newPage();
+            page.on('console', consoleMessage => {
+                if (consoleMessage.type() === 'debug') {
+                    console.debug(`########## ${consoleMessage.text()}`)
+                }
+            });
+            await page.goto('https://syusyu.github.io/spa-manager/src/index.html');
+            await sleep(2000);
         }, timeout);
 
         afterAll(async () => {
@@ -15,12 +23,16 @@ describe(
 
 
         it('Load list page', async () => {
-            let text = await page.evaluate(() => document.body.textContent);
-            expect(text).toContain('プランの変更予約をする');
+            const subTitle = await page.evaluate(() => {
+                return document.querySelector('.list.visible > h3').textContent;
+            });
+            expect(subTitle).toContain('プランの変更予約をする');
 
-            let elm = await page.$('#devices');
-            console.log('### elem=' + elm);
+            const deviceLabels = await page.evaluate(() => {
+                return Array.from(document.querySelector('#devices')).filter(e => e.getAttribute('data-bind-id').includes('$')).map(e => e.label);
+            });
+            expect(deviceLabels.sort()).toEqual(["Please select", "Device title1 of plan1", "Device title2 of plan1"].sort());
         })
     },
     timeout
-)
+);
